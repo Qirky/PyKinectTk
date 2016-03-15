1d17825590f3af49881fe70b8f88f3d8b06e4205
FoxDot <ryankirkbride26@gmail.com>
2016-03-15 23:43:20 +0000

    Prep. for RTD

--- ex-playback.py
+++
@@ -1,9 +0,0 @@
-"""
-    Example script for extracting Kinect Data from Kinect Studio
-"""
-
-if __name__ == "__main__":
-
-    from PyKinectXEF import Playback
-
-    App = Playback.KinectDataSelect()
